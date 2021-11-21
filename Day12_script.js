9e09b83c3aebfee9f5039ce48bf725d1c8f87087
Simi-Paxleal-J <paxsimileal@gmail.com>
2021-11-21 07:45:53 +0530

    Day 12-Basics of Javascript

---
+++ Day12_script.js
@@ -0,0 +1,45 @@
+var c=2;
+c = 5;
+const d = 3;
+/*d = 4; cant assign another value for the constant variable*/
+
+/*Condition Statments*/
+var x = 0;
+if (x>0)
+{
+  console.log("This is the positive number");
+}
+else if(x<0)
+{
+  console.log("This is the negative number");
+}
+else {
+  console.log("It is the zero")
+}
+
+var a = 4, b = 3;
+var ans = a/b;
+console.log("the two values are divided",ans);
+
+/* && || */
+/*&& -> (condition1 && condition2)*/
+var a1 = 0, b1 =1;
+if (a1 && b1)
+{
+  console.log("TRUE");
+}
+else {
+  console.log("FALSE");
+}
+
+/*===*/
+var a2 =1;
+var b2 ="1";
+if (a2 === b2)
+{
+  console.log("same");
+}
+else {
+  console.log("not same");
+}
+/**/
